3bf04361fcd9762a694904d16f38dd90511ee692
Scott Hargrove <scott7c1@gmail.com>
2020-10-12 12:27:59 -0400

    Add Product Currently on profile page

--- app/src/main/java/com/example/pacemarketplace/MainActivity.java
+++ app/src/main/java/com/example/pacemarketplace/MainActivity.java
@@ -6,7 +6,14 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -14,11 +21,41 @@ import java.util.HashSet;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imageView;
+    Button button;
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Upload image Test
+        /*
+        imageView = (ImageView)findViewById(R.id.imageView);
+        button = (Button)findViewById(R.id.buttonLoadPicture);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+    }
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
+
+         */
+
+        //Nav Bar for main activity
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         NavController navController = Navigation.findNavController(this,R.id.fragment);
 
@@ -35,6 +72,9 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration);
 
     */
+
+
+
     }
 
 }
--- app/src/main/java/com/example/pacemarketplace/Profile.java
+++ app/src/main/java/com/example/pacemarketplace/Profile.java
@@ -15,6 +15,8 @@ import android.view.ViewGroup;
  */
 public class Profile extends Fragment {
 
+
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
