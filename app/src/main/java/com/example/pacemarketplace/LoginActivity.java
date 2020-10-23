ff7151e4e66c07a4da824c538aea618d6f48d054
kayleonard00 <70547647+kayleonard00@users.noreply.github.com>
2020-10-23 19:46:01 +0000

    Update LoginActivity.java

--- app/src/main/java/com/example/pacemarketplace/LoginActivity.java
+++ app/src/main/java/com/example/pacemarketplace/LoginActivity.java
@@ -75,7 +75,7 @@ public class LoginActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                            startActivity(new Intent(getApplicationContext(),LandingActivity.class));
                         }else {
                             Toast.makeText(LoginActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                             progressBar.setVisibility(View.GONE);
