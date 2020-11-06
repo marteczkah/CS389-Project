c5923461e574fd40b935a86ddacae80a7c476fee
Marta <48037981+marteczkah@users.noreply.github.com>
2020-11-06 11:47:54 -0500

    Implemented user products page

--- app/src/main/java/com/example/pacemarketplace/UserProductsPage.java
+++ app/src/main/java/com/example/pacemarketplace/UserProductsPage.java
@@ -1,13 +1,32 @@
 package com.example.pacemarketplace;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserProductsPage extends Fragment {
+
+    FirebaseFirestore database = FirebaseFirestore.getInstance();
+    FirebaseAuth fAuth;
+    RecyclerViewAdapter recyclerViewAdapter;
+    RecyclerView rv;
 
     public UserProductsPage() {
         //required empty constructor
@@ -16,6 +35,43 @@ public class UserProductsPage extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.your_products_page, container, false);
+        final ArrayList<Product> userProducts = new ArrayList<>();
+        final Context context = getContext();
+        fAuth = FirebaseAuth.getInstance();
+        String userID = fAuth.getCurrentUser().getUid();
+        rv = v.findViewById(R.id.recycler_view_userproducts);
+        final DocumentReference docRef = database.collection("Users").document(userID);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                DocumentSnapshot document = task.getResult();
+                List<String> favoritesID = (List<String>) document.get("userProducts");
+                int count = 0;
+                for (String id : favoritesID) {
+                    final int countValue = count;
+                    database.collection("Products").document(id).get()
+                            .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                    DocumentSnapshot document = task.getResult();
+                                    String productName = document.get("name").toString();
+                                    String productDescription = document.get("description").toString();
+                                    String price = document.get("price").toString();
+                                    String productID = document.get("productID").toString();
+                                    String sellerID = document.get("sellerID").toString();
+                                    Product product = new Product(productName, price, productDescription, productID, sellerID);
+                                    userProducts.add(countValue, product);
+                                    recyclerViewAdapter = new RecyclerViewAdapter(userProducts, context);
+                                    rv.setAdapter(recyclerViewAdapter);
+                                }
+                            });
+                    count++;
+                }
+            }
+        });
+        recyclerViewAdapter = new RecyclerViewAdapter(userProducts, context);
+        rv.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rv.setAdapter(recyclerViewAdapter);
         return v;
     }
 }
