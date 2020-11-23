a1bc21da647a7525fad032d5c2eed1ded91e1997
kayleonard00 <70547647+kayleonard00@users.noreply.github.com>
2020-11-23 15:04:27 +0000

    Settings (has errors)

--- app/src/main/java/com/example/pacemarketplace/SettingsPage.java
+++ app/src/main/java/com/example/pacemarketplace/SettingsPage.java
@@ -1,4 +1,4 @@
-package com.example.pacemarketplace;
+/*package com.example.pacemarketplace;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -17,5 +17,77 @@ public class SettingsPage extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.account_settings_page, container, false);
         return v;
+    }
+}*/
+package com.example.pacemarketplace;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.example.pacemarketplace.R;
+import com.example.pacemarketplace.databinding.ActivitySettingsBinding;
+import com.example.pacemarketplace.Profile;
+
+import java.util.Objects;
+
+import static java.util.Objects.requireNonNull;
+
+public class SettingsPage extends AppCompatActivity {
+
+    private ActivitySettingsBinding binding;
+    private FirebaseUser firebaseUser;
+    private FirebaseFirestore firestore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.account_settings_page);
+
+        firestore = FirebaseFirestore.getInstance();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser!=null){
+            getInfo();
+        }
+        initClickAction();
+
+    }
+
+    private void initClickAction() {
+        binding.lnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SettingsPage.this, Profile.class));
+            }
+        });
+    }
+
+    private void getInfo(){
+        firestore.collection("Users").document(firebaseUser.getUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                String userName = requireNonNull(documentSnapshot.get("fName")).toString();
+                binding.tvUsername.setText(userName);
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("Get Data", "onFailure: "+e.getMessage());
+            }
+        });
     }
 }
